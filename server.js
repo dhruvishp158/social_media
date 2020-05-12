3808343d524a9e75b039208454d350f7ad2729c0
dhruvish patel <dhruvishp158@gmail.com>
2020-05-12 15:34:10 -0400

    Completed

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require("path");
 const app = express();
 //connect database
 connectDB();
 //Init middleware i.e=BodyParser
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
 
 //Define routes from api folder
 app.use("/api/users", require("./routes/api/users"));
@@ -17,5 +13,13 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+//serve static assesa and production: heroku
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
